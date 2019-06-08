98d1dd298ddc1cfee2e382f3dec98c5ab7b61227
haozhang96 <haozhang@gmx.com>
2019-06-08 16:54:48 -0400

    Fix backend

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -29,64 +29,34 @@ class RegisterUser(View):
             data = json.loads(request.body.decode("utf-8"))
 
             # Make sure that the username has not already been taken.
-            if User.objects.filter(username=data["username"]).count():
+            if User.objects.filter(username=data["userName"]).count():
                 return HttpResponseBadRequest(
                     "The username has already been taken!"
                 )
 
             # Call the email validator which will raise an error if it's
             #   invalid.
-            validate_email(data["userName"])
+            # validate_email(data["userName"])
 
-            # Create the address model instance.
-            address = Address.objects.get_or_create(
-                address=data["addressNameFirst"],
-                city=data["addressNameLast"],
-                state=data["addressState"],
-                zip_code=data["addressZip"],
-            )
-
-            billing_address = Address.objects.get_or_create(
-                address=data["billingAddressStreet"],
-                city=data["billingAddressCity"],
-                state=data["billingAddressState"],
-                zip_code=data["billingAddressZip"],
-            )
-
-            # Create the billing info model instance.
-            billing_info = BillingInfo.objects.get_or_create(
-                first_name=data["billingAddressNameFirst"],
-                last_name=data["billingAddressNameFirst"],
-                card_number=data["billingCardNumber"],
-                expiration_date=data["billingCardExpiration"],
-                address=billing_address,
-                phone_number=data["billingPhone"],
-            )
+            address, billing_address, billing_info = createUserInfo(data)
 
             # Create the user model instance.
-            user = User.objects.create(
+            user = User(
                 username=data["userName"],
                 email=data["email"],
                 password=make_password(data["password"]),
-                first_name=data["nameFirst"],
-                last_name=data["nameLast"],
+                first_name=data["firstName"],
+                last_name=data["lastName"],
                 phone_number=data["phone"],
                 address=address,
                 billing_info=billing_info,
             )
-
-            # Save the model instances to the database.
-            address.save()
-            billing_address.save()
-            billing_info.save()
             user.save()
 
             # User registration was successful. Return their email address.
-            return HttpResponse(status_code=200)
+            return HttpResponse(status=200)
         except json.JSONDecodeError:
             return HttpResponseBadRequest("Invalid JSON body!")
-        except validate_email.ValidationError:
-            return HttpResponseBadRequest("Invalid email!")
 
 class UpdateUser(View):
     def patch(self, request, *args, **kwargs):
@@ -96,52 +66,52 @@ class UpdateUser(View):
 
             # Call the email validator which will raise an error if it's
             #   invalid.
-            validate_email(username)
+            # validate_email(data["userName"])
 
-            # Create the address model instance.
-            address = Address.objects.get_or_create(
-                address=data["addressNameFirst"],
-                city=data["addressNameLast"],
-                state=data["addressState"],
-                zip_code=data["addressZip"],
-            )
-
-            billing_address = Address.objects.get_or_create(
-                address=data["billingAddressStreet"],
-                city=data["billingAddressCity"],
-                state=data["billingAddressState"],
-                zip_code=data["billingAddressZip"],
-            )
-
-            # Create the billing info model instance.
-            billing_info = BillingInfo.objects.get_or_create(
-                first_name=data["billingAddressNameFirst"],
-                last_name=data["billingAddressNameFirst"],
-                card_number=data["billingCardNumber"],
-                expiration_date=data["billingCardExpiration"],
-                address=billing_address,
-                phone_number=data["billingPhone"],
-            )
+            address, billing_address, billing_info = createUserInfo(data)
 
             # Update the user model instance.
-            user = User.objects.get(username=data["username"])
-            user.email=data["email"],
-            user.password=make_password(data["password"]),
-            user.first_name=data["nameFirst"],
-            user.last_name=data["nameLast"],
-            user.address=address,
-            user.phone_number=data["phone"],
-            user.billing_info=billing_info,
-            
-            # Save the model instances to the database.
-            address.save()
-            billing_address.save()
-            billing_info.save()
+            user = User.objects.get(username=data["userName"])
+            user.email=data["email"]
+            user.password=make_password(data["password"])
+            user.first_name=data["firstName"]
+            user.last_name=data["lastName"]
+            user.address=address
+            user.phone_number=data["phone"]
+            user.billing_info=billing_info
             user.save()
 
             # User registration was successful. Return their email address.
-            return HttpResponse(status_code=200)
+            return HttpResponse(status=200)
         except json.JSONDecodeError:
             return HttpResponseBadRequest("Invalid JSON body!")
-        except validate_email.ValidationError:
-            return HttpResponseBadRequest("Invalid email!")
+
+def createUserInfo(data):
+    # Create the address model instance.
+    address = Address(
+        address=data["homeAddress"]["street"],
+        city=data["homeAddress"]["city"],
+        state=data["homeAddress"]["state"],
+        zip_code=data["homeAddress"]["zip"],
+    )
+    address.save()
+
+    billing_address = Address(
+        address=data["billingAddress"]["street"],
+        city=data["billingAddress"]["city"],
+        state=data["billingAddress"]["state"],
+        zip_code=data["billingAddress"]["zip"],
+    )
+    billing_address.save()
+
+    # Create the billing info model instance.
+    billing_info = BillingInfo(
+        first_name=data["billingAddress"]["nameFirst"],
+        last_name=data["billingAddress"]["nameLast"],
+        card_number=data["billingCardNumber"],
+        expiration_date=data["billingCardExpiration"],
+        address=billing_address,
+    )
+    billing_info.save()
+
+    return address, billing_address, billing_info
--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_number = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=15)
     address = models.ForeignKey("Address", on_delete=models.CASCADE)
     billing_info = models.ForeignKey("BillingInfo", on_delete=models.CASCADE)
 
@@ -15,12 +15,11 @@ class Address(models.Model):
     address = models.CharField(max_length=255)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
-    zip_code = models.PositiveIntegerField()
+    zip_code = models.CharField(max_length=10)
 
 class BillingInfo(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    card_number = models.PositiveIntegerField()
-    expiration_date = models.DateTimeField()
+    card_number = models.CharField(max_length=20)
+    expiration_date = models.CharField(max_length=20) # models.DateTimeField()
     address = models.ForeignKey("Address", on_delete=models.CASCADE)
-    phone_number = models.PositiveIntegerField()
