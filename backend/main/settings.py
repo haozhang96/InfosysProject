c53b3ce5e6ac7f4a9141538aa7dcc317b2babad1
haozhang96 <haozhang@gmx.com>
2019-06-08 15:48:46 -0400

    Make models and endpoints

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Change the default user model.
+
+AUTH_USER_MODEL = 'api.User'
